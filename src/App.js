2097515e2f26e9c05f3a83260c8928953ea67d87
Divyam_Sharma <54996898+divyamsharma822@users.noreply.github.com>
2023-09-26 23:32:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React world
+          First Deployment using Jenkins
         </a>
       </header>
     </div>
